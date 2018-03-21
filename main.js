ad097703b698b8857d2fa2502d2c97cfb4fc70cf
David Hull <david.hull1@utexas.edu>
2018-03-21 11:06:52 -0500

    Route and Query Parameters Exercises

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 
-var fs = require('fs');
+/*var fs = require('fs');
 
 var count = 0;
 
@@ -12,12 +12,61 @@ console.log(newLine.length-1);
 
 
 
+//Example Function
+var http = require('http');
+
+var myServer = http.createServer(function(request, response){
+    response.writeHead(200, {'Content-Type': 'text/html'});  //==> //To 		send back something to client
+
+    response.write('<h1>Digital Crafts</h1>'); //=> writes digital crafts to our webpage
+    response.end();
+
+});
+
+//Asking server to listen on a specific port
+myServer.listen(4000);
+
+
+*/
+
+var express = require('express');
+var app = express();
+app.get('/', function(req, res) {
+    res.send('<h1>Hello, World!</h1>');
+});
+
+app.get('/cats', function(req, res) {
+    res.send('<h1>Meow</h1>');
+});
+
+app.get('/dogs', function(req, res) {
+    res.send('<h1>Woof</h1>');
+});
+
+
+app.get('/cats_and_dogs', function(req, res) {
+    res.send('<h1>Living Together</h1>');
+});
+
+//Route Route Parameters
+app.get('/greet/:name', function(req,res){
+    var name = req.params.name || 'world';
+    name = name.substr(1, name.length);
+    res.send('Hello, ' + name + '!')
+});
 
 
 
+//Query Parameters Example
+app.get('/year', function(req, res){
+    var age = req.query.age || 'you old as fuck';
+    var born = 2018 - Number(age);
+    res.send('You were born in ' + born);
+});
 
-
-
+app.listen(9000, function(){
+    console.log('Example app listening on port 9000!');
+});
 
 
 /*var fs = require('fs');
