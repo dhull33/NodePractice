253c5de2d24481a75ddfab7f8a2999fbf1ab125a
David Hull <david.hull1@utexas.edu>
2018-03-30 15:15:23 -0500

    Started AnimalApp

---
+++ AnimalApp/app2.js
@@ -0,0 +1,16 @@
+
+// Includes HTTP & Express libraries
+let http = require('http');
+let express = require('express');
+// Create an instance of express
+let app = express();
+
+//Create server
+http.createServer(app).listen(9000, function(){
+    console.log("Listening on port 9000!");
+});
+
+//Route for the home page
+app.get('/', function(request,response){
+    response.send("<h1>HEY YO BITCH! WHATCHA DOING?</h1>");
+});
